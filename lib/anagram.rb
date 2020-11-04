e589cdee2a164713cb30e8116f18eda47930018c
Yehuda Bortz <yehudabortz@icloud.com>
2020-11-03 20:06:25 -0600

    commit tree 3865580c2f58d3d7c277edafe89c12620569a544

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+require 'pry'
+ class Anagram
+    attr_accessor :word
+
+    def initialize(word_ph)
+        @word = word_ph  
+    end
+    
+    def match(array)
+        matched_array = []
+        array.each do |item|
+            if item.split("").sort == word.split("").sort
+                matched_array.push(item)
+            end
+        end
+        matched_array
+    end
+
+ end
